aa9adaff68f13d4f999e9fe329aa593c30890d9b
AbdallahRustom <abdallah.rustom.ar@gmail.com>
2023-10-07 02:35:02 +0300

    update1

--- config/config.go
+++ config/config.go
@@ -15,46 +15,46 @@ var Data = getConfig()
 type Config struct {
 	GNodeB struct {
 		ControlIF struct {
-			Ip   string `yaml: "ip"`
-			Port int    `yaml: "port"`
-		} `yaml: "controlif"`
+			Ip   string `yaml:"ip"`
+			Port int    `yaml:"port"`
+		} `yaml:"controlif"`
 		DataIF struct {
-			Ip   string `yaml: "ip"`
-			Port int    `yaml: "port"`
-		} `yaml: "dataif"`
+			Ip   string `yaml:"ip"`
+			Port int    `yaml:"port"`
+		} `yaml:"dataif"`
 		PlmnList struct {
-			Mcc   string `yaml: "mmc"`
-			Mnc   string `yaml: "mnc"`
-			Tac   string `yaml: "tac"`
-			GnbId string `yaml: "gnbid"`
-		} `yaml: "plmnlist"`
+			Mcc   string `yaml:"mmc"`
+			Mnc   string `yaml:"mnc"`
+			Tac   string `yaml:"tac"`
+			GnbId string `yaml:"gnbid"`
+		} `yaml:"plmnlist"`
 		SliceSupportList struct {
-			Sst string `yaml: "sst"`
-			Sd  string `yaml: "sd"`
-		} `yaml: "slicesupportlist"`
+			Sst string `yaml:"sst"`
+			Sd  string `yaml:"sd"`
+		} `yaml:"slicesupportlist"`
 	} `yaml:"gnodeb"`
 	Ue struct {
-		Msin  string `yaml: "msin"`
-		Key   string `yaml: "key"`
-		Opc   string `yaml: "opc"`
-		Amf   string `yaml: "amf"`
-		Sqn   string `yaml: "sqn"`
-		Dnn   string `yaml: "dnn"`
+		Msin  string `yaml:"msin"`
+		Key   string `yaml:"key"`
+		Opc   string `yaml:"opc"`
+		Amf   string `yaml:"amf"`
+		Sqn   string `yaml:"sqn"`
+		Dnn   string `yaml:"dnn"`
 		Hplmn struct {
-			Mcc string `yaml: "mcc"`
-			Mnc string `yaml: "mnc"`
-		} `yaml: "hplmn"`
+			Mcc string `yaml:"mcc"`
+			Mnc string `yaml:"mnc"`
+		} `yaml:"hplmn"`
 		Snssai struct {
-			Sst int    `yaml: "sst"`
-			Sd  string `yaml: "sd"`
-		} `yaml: "snssai"`
+			Sst int    `yaml:"sst"`
+			Sd  string `yaml:"sd"`
+		} `yaml:"snssai"`
 	} `yaml:"ue"`
 	AMF struct {
-		Ip   string `yaml: "ip"`
-		Port int    `yaml: "port"`
+		Ip   string `yaml:"ip"`
+		Port int    `yaml:"port"`
 	} `yaml:"amfif"`
 	Logs struct {
-		Level int `yaml: "level"`
+		Level int `yaml:"level"`
 	} `yaml:"logs"`
 }
 
