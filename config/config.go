446c2845f72e3f322d135fa9150b4495f265f1f5
AbdallahRustom <abdallah.rustom.ar@gmail.com>
2023-10-07 04:05:37 +0300

    update11

--- config/config.go
+++ config/config.go
@@ -13,50 +13,51 @@ import (
 var Data = getConfig()
 
 type Config struct {
-	GNodeB struct {
-		ControlIF struct {
-			Ip   string `yaml: "ip"`
-			Port int    `yaml: "port"`
-		} `yaml: "controlif"`
-		DataIF struct {
-			Ip   string `yaml: "ip"`
-			Port int    `yaml: "port"`
-		} `yaml: "dataif"`
-		PlmnList struct {
-			Mcc   string `yaml: "mmc"`
-			Mnc   string `yaml: "mnc"`
-			Tac   string `yaml: "tac"`
-			GnbId string `yaml: "gnbid"`
-		} `yaml: "plmnlist"`
-		SliceSupportList struct {
-			Sst string `yaml: "sst"`
-			Sd  string `yaml: "sd"`
-		} `yaml: "slicesupportlist"`
-	} `yaml:"gnodeb"`
-	Ue struct {
-		Msin  string `yaml: "msin"`
-		Key   string `yaml: "key"`
-		Opc   string `yaml: "opc"`
-		Amf   string `yaml: "amf"`
-		Sqn   string `yaml: "sqn"`
-		Dnn   string `yaml: "dnn"`
-		Hplmn struct {
-			Mcc string `yaml: "mcc"`
-			Mnc string `yaml: "mnc"`
-		} `yaml: "hplmn"`
-		Snssai struct {
-			Sst int    `yaml: "sst"`
-			Sd  string `yaml: "sd"`
-		} `yaml: "snssai"`
-	} `yaml:"ue"`
-	AMF struct {
-		Ip   string `yaml: "ip"`
-		Port int    `yaml: "port"`
-	} `yaml:"amfif"`
-	Logs struct {
-		Level int `yaml: "level"`
-	} `yaml:"logs"`
+    GNodeB struct {
+        ControlIF struct {
+            Ip   string `yaml:"ip"`
+            Port int    `yaml:"port"`
+        } `yaml:"controlif"`
+        DataIF struct {
+            Ip   string `yaml:"ip"`
+            Port int    `yaml:"port"`
+        } `yaml:"dataif"`
+        PlmnList struct {
+            Mcc   string `yaml:"mcc"`
+            Mnc   string `yaml:"mnc"`
+            Tac   string `yaml:"tac"`
+            GnbId string `yaml:"gnbid"`
+        } `yaml:"plmnlist"`
+        SliceSupportList struct {
+            Sst string `yaml:"sst"`
+            Sd  string `yaml:"sd"`
+        } `yaml:"slicesupportlist"`
+    } `yaml:"gnodeb"`
+    Ue struct {
+        Msin  string `yaml:"msin"`
+        Key   string `yaml:"key"`
+        Opc   string `yaml:"opc"`
+        Amf   string `yaml:"amf"`
+        Sqn   string `yaml:"sqn"`
+        Dnn   string `yaml:"dnn"`
+        Hplmn struct {
+            Mcc string `yaml:"mcc"`
+            Mnc string `yaml:"mnc"`
+        } `yaml:"hplmn"`
+        Snssai struct {
+            Sst int    `yaml:"sst"`
+            Sd  string `yaml:"sd"`
+        } `yaml:"snssai"`
+    } `yaml:"ue"`
+    AMF struct {
+        Ip   string `yaml:"ip"`
+        Port int    `yaml:"port"`
+    } `yaml:"amfif"`
+    Logs struct {
+        Level int `yaml:"level"`
+    } `yaml:"logs"`
 }
+
 
 func RootDir() string {
 	log.SetLevel(log.DebugLevel) 
@@ -73,9 +74,7 @@ func getConfig() Config {
 	if err != nil {
 		log.Fatal("Could not find config in: ", configPath)
 	}
-	log.Debug("Hello", string(configPath))
 	file, err := ioutil.ReadFile(configPath)
-	log.Debug("Hello", string(file))
 	log.Debug("YAML content:", string(file))
 	err = yaml.Unmarshal([]byte(file), &cfg)
 	if err != nil {
